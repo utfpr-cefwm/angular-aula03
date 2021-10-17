7c95e07a8b191b253c1317020f9840a8b2aa9367
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-10-17 16:59:56 +0000

    Gera `HomeComponent`.
    - Comando:
      npx ng generate @schematics/angular:component --name=components/home --project=web --no-interactive

---
+++ apps/web/src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+
+@Component({
+  selector: 'artigaria-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+
+  constructor(
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
