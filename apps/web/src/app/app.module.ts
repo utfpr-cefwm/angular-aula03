a9a7940e89b33e0b3bc83c371002f47cd8838ba4
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-10-17 19:33:21 +0000

    Introduz `ArtigoService.getAll`.

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MenubarModule } from 'primeng/menubar';
 
@@ -18,6 +19,7 @@ import { ListaArtigosComponent } from './components/lista-artigos/lista-artigos.
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     MenubarModule,
   ],
--- apps/web/src/app/services/lista-artigos/lista-artigos.service.ts
+++ apps/web/src/app/services/lista-artigos/lista-artigos.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import {
+  map,
+} from 'rxjs/operators';
+
+import {
+  Artigo,
+  IArtigo,
+} from '@artigaria/common';
 
 @Injectable({
   providedIn: 'root',
@@ -6,7 +17,22 @@ import { Injectable } from '@angular/core';
 export class ListaArtigosService {
 
   constructor(
+    private httpClient: HttpClient,
   ) {
+  }
+
+  public getAll(): Observable<Artigo[]> {
+    return this.httpClient.get<IArtigo[]>(`http://localhost:3333/api/artigos`).pipe(
+      map((iArtigos: IArtigo[]) => {
+        return iArtigos.map((iArtigo: IArtigo) => new Artigo(
+          iArtigo._id,
+          iArtigo.imagem,
+          iArtigo.titulo,
+          iArtigo.descricao,
+          iArtigo.url,
+        ));
+      }),
+    );
   }
 
 }
