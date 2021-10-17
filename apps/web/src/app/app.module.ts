d09066ca4fa900bd17943b943a6685c984ebd501
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-10-17 19:53:00 +0000

    Lista os artigos disponíveis.

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +24,8 @@ import { ListaArtigosComponent } from './components/lista-artigos/lista-artigos.
     HttpClientModule,
     AppRoutingModule,
     MenubarModule,
+    CardModule,
+    ButtonModule,
   ],
   providers: [
   ],
--- apps/web/src/app/components/lista-artigos/lista-artigos.component.ts
+++ apps/web/src/app/components/lista-artigos/lista-artigos.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { Artigo } from '@artigaria/common';
+
+import { ListaArtigosService } from '../../services/lista-artigos/lista-artigos.service';
+
 @Component({
   selector: 'artigaria-lista-artigos',
   templateUrl: './lista-artigos.component.html',
@@ -7,7 +13,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaArtigosComponent implements OnInit {
 
+  public artigos$: Observable<Artigo[]> = this.listaArtigosService.getAll();
+
   constructor(
+    private listaArtigosService: ListaArtigosService,
   ) {
   }
 
