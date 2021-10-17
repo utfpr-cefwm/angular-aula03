d9d0b8de75c904d7f52b2392272b5fd24e074447
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-10-17 20:09:32 +0000

    Gera `ArtigoComponent`.
    - Comando:
      npx ng generate @schematics/angular:component --name=modules/artigo-edicao/components/artigo --project=web --module=modules/artigo-edicao --export --no-interactive

---
+++ apps/web/src/app/modules/artigo-edicao/components/artigo/artigo.component.ts
@@ -0,0 +1,20 @@
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+
+@Component({
+  selector: 'artigaria-artigo',
+  templateUrl: './artigo.component.html',
+  styleUrls: ['./artigo.component.css'],
+})
+export class ArtigoComponent implements OnInit {
+
+  constructor(
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
