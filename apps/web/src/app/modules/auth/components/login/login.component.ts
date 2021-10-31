3a4ef3aa66ecf9d92f5c310a4988b7ca1b4b4143
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-10-31 18:57:36 +0000

    Submete formulário de login do usuário.

--- apps/web/src/app/modules/auth/components/login/login.component.ts
+++ apps/web/src/app/modules/auth/components/login/login.component.ts
@@ -1,7 +1,26 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import {
   Component,
   OnInit,
 } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import {
+  ActivatedRoute,
+  Router,
+} from '@angular/router';
+
+import { of } from 'rxjs';
+import { catchError, take } from 'rxjs/operators';
+
+import { MessageService } from 'primeng/api';
+
+import { AuthResult, IUsuario } from '@artigaria/common';
+
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'artigaria-login',
@@ -10,11 +29,52 @@ import {
 })
 export class LoginComponent implements OnInit {
 
+  public formGroup: FormGroup = this.formBuilder.group({
+    login: ['', Validators.required],
+    senha: ['', Validators.required],
+  });
+
   constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private messageService: MessageService,
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
   ) {
   }
 
   ngOnInit(): void {
+  }
+
+  public fazerLogin(): void {
+    const body: Pick<IUsuario, 'login' | 'senha'> = this.formGroup.value;
+    this.authService.login(body).pipe(
+      take(1),
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401) {
+          this.messageService.add({
+            severity: 'warn',
+            summary: 'Usuário ou senha inválidos',
+          });
+          return of(undefined);
+        }
+        throw err;
+      }),
+    ).subscribe((result?: AuthResult) => {
+      if (result?.jwt) {
+        this.messageService.add({
+          severity: 'success',
+          summary: `Olá novamente, ${result.usuario.nome}!`,
+          detail: result.usuario.administrador
+          ? 'Seus poderes de administrador foram ativados!'
+          : 'Estamos redirecionando você para o conteúdo...',
+        });
+        window.localStorage.setItem('jwt', result.jwt);
+        setTimeout(() => {
+          this.router.navigate([ '/' ]);
+        }, 1000);
+      }
+    });
   }
 
 }
--- apps/web/src/app/modules/auth/services/auth/auth.service.ts
+++ apps/web/src/app/modules/auth/services/auth/auth.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {
+  Inject,
+  Injectable,
+} from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import {
+  AuthResult,
+  IUsuario,
+} from '@artigaria/common';
 
 @Injectable({
   providedIn: 'root',
@@ -6,7 +17,16 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor(
+    @Inject('API_BASE_URL') private apiBaseUrl: string,
+    private httpClient: HttpClient,
   ) {
+  }
+
+  public login(iUsuario: Pick<IUsuario, 'login' | 'senha'>): Observable<AuthResult> {
+    return this.httpClient.post<AuthResult>(
+      `${this.apiBaseUrl}/auth/login`,
+      iUsuario,
+    );
   }
 
 }
