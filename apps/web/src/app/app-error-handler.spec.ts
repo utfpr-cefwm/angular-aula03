2b6d05618ccaebbc3402a07aa49fdc12f84429af
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-10-31 16:40:09 +0000

    Cria o `AppErrorHandler`.
    - Comando:
      npx ng generate @schematics/angular:class --name=app-error-handler --project=web --no-interactive

---
+++ apps/web/src/app/app-error-handler.spec.ts
@@ -0,0 +1,9 @@
+import { AppErrorHandler } from './app-error-handler';
+
+describe('AppErrorHandler', () => {
+
+  it('should create an instance', () => {
+    expect(new AppErrorHandler()).toBeTruthy();
+  });
+
+});
