4a575b27b97603985e8919286d29a5a5a40abf8d
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-10-31 19:57:55 +0000

    Finaliza a implementação da diretiva customizada.

--- apps/web/src/app/modules/artigo-edicao/directives/src-debounce/src-debounce.directive.ts
+++ apps/web/src/app/modules/artigo-edicao/directives/src-debounce/src-debounce.directive.ts
@@ -1,20 +1,53 @@
-import { Directive, EventEmitter, HostBinding, Input, Output } from '@angular/core';
+import {
+  Directive,
+  HostBinding,
+  Input,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
+
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Directive({
   selector: '[artigariaSrcDebounce]',
 })
-export class SrcDebounceDirective {
+export class SrcDebounceDirective implements OnChanges, OnDestroy {
+
+  private srcDebounced: string = '';
+
+  private src$: Subject<string> = new Subject();
+
+  private subUnsubscribe: Subject<void> = new Subject();
 
   @Input()
   public artigariaSrcDebounce?: string;
 
   constructor(
   ) {
+    this.src$.pipe(
+      takeUntil(this.subUnsubscribe),
+      debounceTime(1000),
+    ).subscribe(
+      (src: string) => this.srcDebounced = src,
+    );
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.artigariaSrcDebounce) {
+      this.src$.next(this.artigariaSrcDebounce);
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.subUnsubscribe.next();
+    this.subUnsubscribe.complete();
   }
 
   @HostBinding('attr.src')
   public get src(): string {
-    return this.artigariaSrcDebounce || '';
+    return this.srcDebounced;
   }
 
 }
