abfd472e8584e55e845700bc673a54f90f287aca
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-10-31 19:45:20 +0000

    Introduz funcionalidade básica de aplicação de `src` na imagem via diretiva.

--- apps/web/src/app/modules/artigo-edicao/directives/src-debounce/src-debounce.directive.ts
+++ apps/web/src/app/modules/artigo-edicao/directives/src-debounce/src-debounce.directive.ts
@@ -1,12 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, EventEmitter, HostBinding, Input, Output } from '@angular/core';
 
 @Directive({
   selector: '[artigariaSrcDebounce]',
 })
 export class SrcDebounceDirective {
 
+  @Input()
+  public artigariaSrcDebounce?: string;
+
   constructor(
   ) {
+  }
+
+  @HostBinding('attr.src')
+  public get src(): string {
+    return this.artigariaSrcDebounce || '';
   }
 
 }
